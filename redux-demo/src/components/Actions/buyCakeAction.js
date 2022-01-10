d738d3f51703f1fe24f39134d6e561398751e45f
Methma Peiris <methma34@gmail.com>
2022-01-10 16:13:50 +0530

    Create a separate action folder and store the first action

--- redux-demo/src/components/Actions/buyCakeAction.js
+++ redux-demo/src/components/Actions/buyCakeAction.js
@@ -1,35 +1,37 @@
-const redux = require('redux')
-const createStore = redux.createStore
+import * as ActionTypes from './index';
 
-const BUY_CAKE = 'BUY_CAKE'
-
-function buyCake(){
+export const buyCakeAction = () => {
     return{
-        type: BUY_CAKE,
+        type: ActionTypes.BUY_CAKE,
         info: 'First redux action'
     }
 }
 
-const initialState = {
-    numberOfCakes: 10
-}
+// const redux = require('redux')
+// const createStore = redux.createStore
 
-const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case BUY_CAKE:
-            return{
-                ...state,
-                numberOfCakes: state.numberOfCakes - 1
-            }
-        default:
-            return state    
-    }
-}
 
-const store = createStore(reducer) //create the redux store
-console.log('Initial state: ', store.getState())
-const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-unsubscribe()
+
+// const initialState = {
+//     numberOfCakes: 10
+// }
+
+// const reducer = (state = initialState, action) => {
+//     switch(action.type){
+//         case BUY_CAKE:
+//             return{
+//                 ...state,
+//                 numberOfCakes: state.numberOfCakes - 1
+//             }
+//         default:
+//             return state    
+//     }
+// }
+
+// const store = createStore(reducer) //create the redux store
+// console.log('Initial state: ', store.getState())
+// const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+// store.dispatch(buyCake())
+// store.dispatch(buyCake())
+// store.dispatch(buyCake())
+// unsubscribe()
