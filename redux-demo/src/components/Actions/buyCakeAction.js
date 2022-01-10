c6a682431f5e125166c0021ca413b50a2f9d4cf7
Methma Peiris <methma34@gmail.com>
2022-01-10 16:19:33 +0530

    Create a separate action folder and store the first reducer function

--- redux-demo/src/components/Actions/buyCakeAction.js
+++ redux-demo/src/components/Actions/buyCakeAction.js
@@ -11,23 +11,6 @@ export const buyCakeAction = () => {
 // const createStore = redux.createStore
 
 
-
-// const initialState = {
-//     numberOfCakes: 10
-// }
-
-// const reducer = (state = initialState, action) => {
-//     switch(action.type){
-//         case BUY_CAKE:
-//             return{
-//                 ...state,
-//                 numberOfCakes: state.numberOfCakes - 1
-//             }
-//         default:
-//             return state    
-//     }
-// }
-
 // const store = createStore(reducer) //create the redux store
 // console.log('Initial state: ', store.getState())
 // const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
---
+++ redux-demo/src/components/Reducers/buyCakeReducer.js
@@ -0,0 +1,17 @@
+import * as types from '../Actions';
+
+const initialState = {
+    numberOfCakes: 10
+};
+
+export default function reducer (state = initialState, action) {
+    switch(action.type){
+        case types.BUY_CAKE:
+            return{
+                ...state,
+                numberOfCakes: state.numberOfCakes - 1
+            }
+        default:
+            return state    
+    }
+}
