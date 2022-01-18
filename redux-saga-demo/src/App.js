cb3fe306e03868513984eef66910648faae0ac43
Methma Peiris <methma34@gmail.com>
2022-01-18 15:26:09 +0530

    Add bootstrap button to App.js

--- redux-saga-demo/src/App.js
+++ redux-saga-demo/src/App.js
@@ -1,4 +1,6 @@
 import {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from "react-bootstrap";
 
 export class App extends Component{
     constructor(props) {
@@ -16,11 +18,11 @@ export class App extends Component{
     render(){
         return(
             <div>
-                <button
+                <Button
                     onClick={this.fetchData}
-                    className="btn btn-primary"
+                    className="btn btn-primary btn-lg"
                 >Fetch
-                </button>
+                </Button>
 
             </div>
         )
