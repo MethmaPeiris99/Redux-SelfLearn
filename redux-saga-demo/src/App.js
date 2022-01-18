146a086404761a24baafa79003eb8f8d49cdad8f
Methma Peiris <methma34@gmail.com>
2022-01-18 14:16:48 +0530

    Change App component to a class component

--- redux-saga-demo/src/App.js
+++ redux-saga-demo/src/App.js
@@ -1,24 +1,13 @@
-import './App.css';
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {getUserAction} from "./Actions/getUserAction";
+import {Component} from "react";
 
-export default function App() {
-    const dispatch = useDispatch();
+export class App extends Component{
+    render(){
+        return(
+            <div>
 
-    useEffect(() => {
-        dispatch(getUserAction());
-    },[dispatch])
-
-    const user = useSelector(state => state.user.user);
-    console.log(user);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+            </div>
+        )
+    }
 }
 
+export default App
