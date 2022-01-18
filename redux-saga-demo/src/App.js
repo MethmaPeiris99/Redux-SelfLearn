259da26102ab16056a808b61d026ab433af14190
Methma Peiris <methma34@gmail.com>
2022-01-18 14:23:37 +0530

    Add a button to implement fetch data function

--- redux-saga-demo/src/App.js
+++ redux-saga-demo/src/App.js
@@ -1,9 +1,26 @@
 import {Component} from "react";
 
 export class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+        this.fetchData = this.fetchData.bind(this)
+    }
+    fetchData() {
+
+    }
+
     render(){
         return(
             <div>
+                <button
+                    onClick={this.fetchData}
+                    className="btn btn-primary"
+                >Fetch
+                </button>
 
             </div>
         )
